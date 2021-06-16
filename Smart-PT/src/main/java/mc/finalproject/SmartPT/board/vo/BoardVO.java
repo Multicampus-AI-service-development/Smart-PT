2f2be08e279585ce006685e49a9c1a6e327cbc14
daehyok <kilcs12@hanmail.net>
2021-06-16 13:17:02 +0900

    Add board part CRUD function in WEbService and BoardVO

--- Smart-PT/src/main/java/mc/finalproject/SmartPT/board/vo/BoardVO.java
+++ Smart-PT/src/main/java/mc/finalproject/SmartPT/board/vo/BoardVO.java
@@ -1,5 +1,37 @@
 package mc.finalproject.SmartPT.board.vo;
 
-public class BoardVO {
+import java.util.Date;
 
+public class BoardVO {
+	private int number; //게시판 고유 번호(PK)
+	private String title; //게시글 제목
+	private String content; //게시글 내용
+	private Date joinDate; //게시글 시간
+	
+	//댓글 기능 구현 해야됨
+	
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
 }
--- Smart-PT/src/main/java/mc/finalproject/SmartPT/web/service/WEBService.java
+++ Smart-PT/src/main/java/mc/finalproject/SmartPT/web/service/WEBService.java
@@ -1,7 +1,22 @@
 package mc.finalproject.SmartPT.web.service;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.zip.DataFormatException;
+
+import mc.finalproject.SmartPT.board.vo.BoardVO;
+
 public interface WEBService {
 
 	//로그인
 	public void login();
+	
+	//게시판
+	public void boardInsert(BoardVO vo)throws DataFormatException; //C
+	public ArrayList<BoardVO> boardList()throws DataFormatException; //R(게시판 목록)
+	public BoardVO boardSelect(int number)throws DataFormatException; //R(특정 게시판 내용 가져오기)
+	public void boardUpdate(BoardVO vo)throws DataFormatException; //U
+	public void boardDelete(int number)throws DataFormatException;//D
+	
+	//각 게시글 별 댓글 CRUD
 }

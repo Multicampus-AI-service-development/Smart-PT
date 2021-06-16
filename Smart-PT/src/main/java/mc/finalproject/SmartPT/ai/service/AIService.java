d0baf9533d3a61c7f43656c7af21d155d5c5bb4d
HoriYeori <xsrsx2000@gmail.com>
2021-06-16 15:06:14 +0900

    AIController, AIService modified. NCLOUD services copy&pasted. pom.xml modified for json and file related libraries(Maven dependency)

--- Smart-PT/src/main/java/mc/finalproject/SmartPT/ai/service/AIService.java
+++ Smart-PT/src/main/java/mc/finalproject/SmartPT/ai/service/AIService.java
@@ -1,5 +1,8 @@
 package mc.finalproject.SmartPT.ai.service;
 
 public interface AIService {
-
+	public String clovaTextToSpeech(String filePathName, String language);
+	public String clovaOCRService(String filePathName);
+	public String clovaSpeechToText(String filePathName, String language);
+	public String translateWords(String words) throws Exception;
 }

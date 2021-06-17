e3fd6f51e8469e01a23c54b87cdb7184aa592b63
junghi1015 <junghi1015@gmail.com>
2021-06-18 01:38:59 +0900

    효인수정

--- Smart-PT/src/main/java/mc/finalproject/SmartPT/web/controller/WEBControllerImpl.java
+++ Smart-PT/src/main/java/mc/finalproject/SmartPT/web/controller/WEBControllerImpl.java
@@ -3,6 +3,7 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import mc.finalproject.SmartPT.web.service.BoardService;
 
@@ -13,9 +14,13 @@ public class WEBControllerImpl implements WEBController{
    BoardService service; //서비스
 
    @Override
-   public String postingMain() throws Exception {
-	   // TODO Auto-generated method stub
-	   return null;
+   @RequestMapping(value = "/boardList.do", method = RequestMethod.GET)
+   public String boardList() throws Exception {
+	   
+	   System.out.println("test");
+	   //service.boardList();
+	   
+	   return "posting/boardList";
    }
    
   

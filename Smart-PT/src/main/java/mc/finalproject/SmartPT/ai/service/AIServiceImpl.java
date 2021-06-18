996ec469ea9d665943d8fbf865c0d480cdf17eb1
paul_mauer <paulmauer37@gmail.com>
2021-06-18 13:25:49 +0900

    tts modify

--- Smart-PT/src/main/java/mc/finalproject/SmartPT/ai/service/AIServiceImpl.java
+++ Smart-PT/src/main/java/mc/finalproject/SmartPT/ai/service/AIServiceImpl.java
@@ -1,5 +1,304 @@
-package mc.finalproject.SmartPT.ai.service;
+package mc.sn.basic.ai.service;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("aiService")
+@Transactional(propagation = Propagation.REQUIRED)
 public class AIServiceImpl implements AIService {
 
+	@Override
+	public String translate(String words) throws DataAccessException {
+		// TODO Auto-generated method stub
+		 StringBuffer res = null;
+		 String clientId = "56t07ba7h3";//�븷�뵆由ъ��씠�뀡 �겢�씪�씠�뼵�듃 �븘�씠�뵒媛�";
+	     String clientSecret = "sBCLaVzbt3V78qGGc52TIjJqxKJ19D8pSwIdgJ4w";//�븷�뵆由ъ��씠�뀡 �겢�씪�씠�뼵�듃 �떆�겕由욧컪";
+	     try {
+	         String text = URLEncoder.encode(words, "UTF-8");
+	         String apiURL = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation";
+	         URL url = new URL(apiURL);
+	         HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	         con.setRequestMethod("POST");
+	         con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+	         con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+	         // post request
+	         String postParams = "source=ko&target=en&text=" + text;
+	         con.setDoOutput(true);
+	         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	         wr.writeBytes(postParams);
+	         wr.flush();
+	         wr.close();
+	         int responseCode = con.getResponseCode();
+	         BufferedReader br;
+	         if(responseCode==200) { // �젙�긽 �샇異�
+	             br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	         } else {  // �삤瑜� 諛쒖깮
+	             br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	         }
+	         String inputLine;
+	         res = new StringBuffer();
+	         while ((inputLine = br.readLine()) != null) {
+	             res.append(inputLine);
+	         }
+	         br.close();
+	         System.out.println("service "+res.toString());
+	     } catch (Exception e) {
+	         System.out.println(e);
+	     }
+		
+	
+		return res.toString();
+	}
+
+	
+	@Override
+	public String clovaSpeechToText(String filePathName, String language) {
+		// TODO Auto-generated method stub
+		String clientId = "p2e6gctdgm";             // Application Client ID";
+        String clientSecret = "iMZJArGrjD17NnTrudNVhjMqKImVW5lAHt49Lxma";     // Application Client Secret";
+        String result = null;
+        try {
+//            String imgFile = filePathName;
+        	String imgFile = "c:/Users/wo779/Downloads/voiceMsg.mp3";		// c:/Users/wo779/Downloads/voiceMsg.mp3 파일을 재생하게 만들었음.
+            File voiceFile = new File(imgFile);
+
+            //String language = "Kor";        // �뼵�뼱 肄붾뱶 ( Kor, Jpn, Eng, Chn )
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + language;
+            URL url = new URL(apiURL);
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/octet-stream");
+            conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            conn.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+
+            OutputStream outputStream = conn.getOutputStream();
+            FileInputStream inputStream = new FileInputStream(voiceFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            inputStream.close();
+            BufferedReader br = null;
+            int responseCode = conn.getResponseCode();
+            if(responseCode == 200) { // �젙�긽 �샇異�
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {  // �삤瑜� 諛쒖깮
+                System.out.println("error!!!!!!! responseCode= " + responseCode);
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            }
+            String inputLine;
+            
+            if(br != null) {
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+                result = response.toString();
+            } else {
+                System.out.println("error !!!");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+      
+		return result;
+	}
+
+
+	@Override
+	public String clovaOCRService(String filePathName) {
+		// TODO Auto-generated method stub
+		String apiURL = "https://cdc0270b45cc4dc4b755630677837d5c.apigw.ntruss.com/custom/v1/9087/b5fdaa8257e21c8088a9bf8b97a9c0e07d2693da0721e30bbdaa5a7622931c4e/infer";
+		String secretKey = "QnpHQ1NoT1BsQmZTRGNlUE13ak1teGRyZ0RiZmhBSXM=";
+		String imageFile = filePathName;
+		StringBuffer response = null;
+		try {
+			URL url = new URL(apiURL);
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
+			con.setReadTimeout(30000);
+			con.setRequestMethod("POST");
+			String boundary = "----" + UUID.randomUUID().toString().replaceAll("-", "");
+			con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+			JSONObject json = new JSONObject();
+			json.put("version", "V2");
+			json.put("requestId", UUID.randomUUID().toString());
+			json.put("timestamp", System.currentTimeMillis());
+			JSONObject image = new JSONObject();
+			image.put("format", "jpg");
+			image.put("name", "demo");
+			JSONArray images = new JSONArray();
+			images.put(image);
+			json.put("images", images);
+			String postParams = json.toString();
+
+			con.connect();
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			long start = System.currentTimeMillis();
+			File file = new File(imageFile);
+			writeMultiPart(wr, postParams, file, boundary);
+			wr.close();
+
+			int responseCode = con.getResponseCode();
+			BufferedReader br;
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+			String inputLine;
+			response = new StringBuffer();
+			while ((inputLine = br.readLine()) != null) {
+				response.append(inputLine);
+			}
+			br.close();
+
+			System.out.println(response);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return response.toString();
+	}
+	
+	private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
+	IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("--").append(boundary).append("\r\n");
+		sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
+		sb.append(jsonMessage);
+		sb.append("\r\n");
+	
+		out.write(sb.toString().getBytes("UTF-8"));
+		out.flush();
+	
+		if (file != null && file.isFile()) {
+			out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
+			StringBuilder fileString = new StringBuilder();
+			fileString
+				.append("Content-Disposition:form-data; name=\"file\"; filename=");
+			fileString.append("\"" + file.getName() + "\"\r\n");
+			fileString.append("Content-Type: application/octet-stream\r\n\r\n");
+			out.write(fileString.toString().getBytes("UTF-8"));
+			out.flush();
+	
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				byte[] buffer = new byte[8192];
+				int count;
+				while ((count = fis.read(buffer)) != -1) {
+					out.write(buffer, 0, count);
+				}
+				fis.close();
+				out.write("\r\n".getBytes());
+			} catch(Exception e) {
+				System.out.println(e);
+			}
+	
+			out.write(("--" + boundary + "--\r\n").getBytes("UTF-8"));
+		}
+		out.flush();
+}
+
+
+	@Override
+	public String clovaTextToSpeech(String filePathName, String language) {
+		// TODO Auto-generated method stub
+		String result = null;
+		String clientId = "dhmge0vn1o";//�븷�뵆由ъ��씠�뀡 �겢�씪�씠�뼵�듃 �븘�씠�뵒媛�";
+        String clientSecret = "nf3rJZkgRdHqyWrEmO3YMhhhmNcMKxoZo3ANRt7X";//�븷�뵆由ъ��씠�뀡 �겢�씪�씠�뼵�듃 �떆�겕由욧컪"        
+        try {
+        	File file = new File("c:/Users/wo779/Downloads/intro.txt");		// c:/Users/wo779/Downloads/intro.txt파일을 재생하게 만듦. 차후 선택적으로 재생되게끔 만들어야 함.
+        	
+        	
+        	FileReader fr = new FileReader(file);
+        	BufferedReader br1 = new BufferedReader(fr);
+        	StringBuffer sb = new StringBuffer();
+        	String temp = null;
+        	while((temp=br1.readLine())!=null) {
+        		sb.append(temp);
+        	}
+        	br1.close();
+        	fr.close();
+        	System.out.println(sb.toString());
+            String text = URLEncoder.encode(sb.toString(), "UTF-8"); // 13�옄
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            // post request
+            String postParams = "speaker="+language+"&volume=0&speed=0&pitch=0&format=mp3&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br= null;
+            if(responseCode==200) { // �젙�긽 �샇異�
+                InputStream is = con.getInputStream();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                // �옖�뜡�븳 �씠由꾩쑝濡� mp3 �뙆�씪 �깮�꽦
+                String tempname = Long.valueOf(new Date().getTime()).toString();
+                result = "tts_"+tempname+".mp3";
+                File f = new File("c:/ai/"+result);
+                f.createNewFile();
+                OutputStream outputStream = new FileOutputStream(f);
+                while ((read =is.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+                outputStream.flush();
+                outputStream.close();
+                is.close();
+            } else {  // �삤瑜� 諛쒖깮
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+                result = response.toString();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+		return result;
+	}
+
+	
 }

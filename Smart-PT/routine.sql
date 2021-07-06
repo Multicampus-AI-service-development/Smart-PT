2266d989c9111404cedba0edf829f34d0c795ed1
junghi1015 <junghi1015@gmail.com>
2021-07-06 15:22:24 +0900

    [db] myroutine, routine sql file push

---
+++ Smart-PT/routine.sql
@@ -0,0 +1,50 @@
+insert into myspring.routine (routineName, engName, imaPath, description) values ('으쓱으쓱 스트레칭', 'Shrug', 'resources/assets/images/routine/shrug.jpg', '어깨를 부드럽게 들어올린후 자연스럽게 툭 떨어뜨린다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('만세 운동', 'Hands up', 'resources/assets/images/routine/handsup.jpg', '정면을 바라보고 양팔을 앞으로 나란히 자세로 자연스럽게 머리위로 들어올린다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('목돌리기 스트레칭', 'Neck turning stretch', 'resources/assets/images/routine/neckStretch.jpg', '양팔을 앞쪽으로 교차해서 쇄골을 잡는다.
+고개를 원을 그리며 목을 돌려준다 한방향에 20초씩 양방향을 해준다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('열중쉬어 스트레칭', 'Stand with arms behind stretch', 'resources/assets/images/routine/handsBack.jpg', '열중쉬어 동작처럼 팔을 등뒤로 깎지를 낀 상태에서 팔을 서서히 들어올려준다.
+이때 상체가 앞으로 숙여지지 않도록 주의한다.
+
+');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('치킨 운동', 'Chicken stretch', 'resources/assets/images/routine/chicken.jpg', '닭 날개처럼 양팔을 벌려서 어깨랑 등을 펴준다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('머리 누르기 스트레칭', 'Head press stretch', 'resources/assets/images/routine/headDown.jpg', '양팔을 턱밑에 대고 고개를 뒤로 젖혀준다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('엎드리기', 'Lying on your stomach(on a pillow)', 'resources/assets/images/routine/waist_1.jpg', '그림처럼 엎드려 4~5분간 깊게 심호흡. 골반과 다리는 완전히 이완, 하루에 6~8번 정도 반복');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('엎드려 팔 펴 윗몸 세우기', 'Prone press-ups', 'resources/assets/images/routine/waist_2.jpg', '그림처럼 동작. 팔을 완전히 편 상태로 2~3초간 유지한 후 원래 자세로 복귀. 
+한 번에 10회 반복, 하루 6~8번 2시간 간격으로 실시. 
+전 동작 간에 다리와 골반 등 하체 완전 이완');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('누워 등 굽히기', 'Lying flexion', 'resources/assets/images/routine/waist_3.jpg', '그림처럼 동작. 
+자세 1~2초간 유지 후 다리 내려 원래 자세로 복귀. 가능한 한 다리를 가슴 가까이 끌어당긴다. 
+한 번에 5~6번 반복, 하루 3~4회 정도 반복. 무릎을 당길 때 긴장 또는 통증이 없다면 중단하고 앉아 등 굽히기 운동으로 전환.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('앉아 등 굽히기', 'Sitting flexion', 'resources/assets/images/routine/waist_4.png', '그림처럼 동작. 1회 5~6번 반복. 하루 3~4회 반복');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('아이소메트릭 로우', 'Isometric rows', 'resources/assets/images/routine/waist_5.jpg', '1. 등받이가 푹신한 의자에 앉는다. 2. 손가락이 앞을 향하고 손바닥이 서로 마주볼 수 있도록 팔을 굽힌다. 
+3. 팔꿈치를 뒤로 보내면서 숨을 내쉬고, 견갑골을 함께 오므려준다. 4. 깊게 심호흡하며 자세를 10초간 유지한다. 
+5. 숨을 내쉬면서 천천히 처음 자세로 돌아온다. 6. 1분간 위 동작을 반복한다.(6회), 7. 하루에 수 회 반복한다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('차일드 포즈', 'Child''s pose', 'resources/assets/images/routine/waist_6.jpg', '1. 손과 무릎을 땅에 대고 엉덩이를 뒤꿈치에 기대듯 뒤로 기울인다. 2. 몸을 앞으로 접으면서 엉덩이를 구부리고, 손을 앞쪽으로 내민다. 
+3. 아랫배를 허벅지에 대고 쉰다. 4. 손바닥을 위로 향하게 하면서 몸 앞으로 팔을 쭉 뻗는다. 5. 깊게 심호흡하는 데에 집중하고, 몸 전반에 걸친 긴장을 풀어준다. 
+6. 최대 1분까지 자세를 유지한다.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('이상근 스트레칭', 'Piriformis stretch', 'resources/assets/images/routine/pelvis_1.jpg', '1. 바닥에 앉아 양 다리를 앞으로 뻗는다.
+2. 오른 다리를 왼쪽으로 넘기고 발로 바닥을 짚는다.
+3. 오른 손은 몸 뒤쪽 바닥에 두고, 왼 팔꿈치를 오른 무릎에 대고 왼쪽으로 밀어주며 몸통을 오른쪽으로 틀어준다. 
+4. 만약 허리가 아프다면 왼손으로 오른 무릎을 당겨주며 몸통을 틀어준다.
+5. 반대도 동일하게 실시.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('런지 앤 트위스트', 'Lunge with spinal twist', 'resources/assets/images/routine/pelvis_2.jpg', '1. 양발로 선 상태에서 왼발을 크게 앞으로 디딘다.
+2. 왼 무릎을 구브리며 런지 자세를 취한다. 이때 오른발 끝은 바닥을 지지하고 다리는 편 상태를 유지한다.
+3. 오른손으로 바닥을 짚고 상체를 왼쪽으로 뒤틀며 왼팔을 천장을 향해 뻗어준 상태로 30초에서 2분간 유지한다.
+4. 반대도 동일하게 실시');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('고관절 굴곡근 스트레칭', 'Lunging hip flexor stretch', 'resources/assets/images/routine/pelvis_3.jpg', '1. 왼 무릎을 바닥에 대고 오른발을 앞으로 디디고 무릎을 구부린다.
+2. 몸을 앞으로 기대고 왼쪽 골반을 바닥으로 뻗어 늘려준다.
+3. 엉덩이를 쥐어짜듯 힘을 준 채로 30초에서 2분간 유지한다.
+4. 반대도 동일하게 실시');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('4자 스트레칭', 'Figure four stretch', 'resources/assets/images/routine/pelvis_4.jpg', '1. 등을 기대 누워서 무릎을 굽혀 발로 바닥을 짚는다.
+2. 왼발을 교차시켜 오른쪽 무릎 위쪽으로 얹는다.
+3. 오른 다리를 바닥에서 들어올리며 양손으로 오른 다리 뒤쪽을 잡아 가슴쪽으로 부드럽게 당겨준다.
+4. 편안한 상태에서 30초에서 2분간 유지한다.
+5. 반대도 동일하게 실시');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('프레첼 스트레칭', 'Pretzel stretch', 'resources/assets/images/routine/pelvis_5.jpg', '');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('비둘기 스트레칭', 'Pigeon stretch', 'resources/assets/images/routine/pelvis_6.jpg', '');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('논산 훈련소', 'Nonsan', 'resources/assets/images/routine/core_1.jpg', '');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('양말 스케이팅', 'Skating of socks', 'resources/assets/images/routine/core_2.jpg', '');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('버드독 + 흉추 가동술', 'Enhanced Birddog', 'resources/assets/images/routine/core_3.png', '');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('고양이 자세', 'Cat and Cow', 'resources/assets/images/routine/core_4.jpg', '효과 - 복부를 강하게 당겨주는 동작으로 코어근육 강화에 도움이 됨.');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('데드 버그', 'Dead Bug', 'resources/assets/images/routine/core_5.png', '효과 - 복근 및 코어근육 강화에 효과적');
+insert into myspring.routine (routineName, engName, imaPath, description) values ('러시안 트위스트', 'Russian Twist', 'resources/assets/images/routine/core_6.jpg', '효과 - 복부를 골고루 강화시키고 특히 외복사근 강화에 효과적인 운동');
